df253ad60235242e122b08fcc8627c4d98f42aa9
dennyzain <abbasdenny24@gmail.com>
2022-05-24 17:06:56 +0700

    chore: RTK and react-redux

--- src/components/organisms/SearchSurah.tsx
+++ src/components/organisms/SearchSurah.tsx
@@ -22,18 +22,22 @@ const SearchSurah: React.FC = () => {
     searchFetching();
   };
 
+  const handleCloseSuggest = () => {
+    console.log('clg');
+  };
+
   return (
-    <div className="flex justify-center relative">
+    <div className="relative flex flex-col mx-5">
       <input
         type="text"
         placeholder="Nama Surat"
         className="border border-black p-1 font-inter font-semibold"
         onChange={handleInput}
       />
-      <div className="absolute">
+      <div className="absolute top-full z-50 flex flex-col left-0 right-0">
         {resultFilter && resultFilter.map((item:DetailSurahProps) => (
-          <Link href={`surah/${item.number}`}>
-            <button type="button" className="p-2 block w-full">
+          <Link href={`/surah/${item.number}`}>
+            <button key={item.number} type="button" className="border border-slate-800 p-2 hover:bg-blue-300" onClick={handleCloseSuggest}>
               <p>
                 {item.name.transliteration.id}
               </p>
