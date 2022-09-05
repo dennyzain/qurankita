892211b9bcd72fd897967fd8c013b1450e89a82d
dennyzain <abbasdenny24@gmail.com>
2022-09-05 15:20:44 +0700

    fix: types all responses API

--- src/types/index.types.ts
+++ src/types/index.types.ts
@@ -1,3 +1,38 @@
 export interface DataProps{
     name:string;
 }
+
+export interface ListDataSurahTypes{
+    nomor: number,
+    nama: string,
+    nama_latin:string,
+    jumlah_ayat: number,
+    tempat_turun: string,
+    arti: string,
+    deskripsi:string,
+    audio: string
+}
+
+export interface AyahSurahTypes{
+    id: number,
+    surah: number,
+    nomor: number,
+    ar: string,
+    tr: string,
+    idn: string
+}
+export interface DetailDataSurahTypes{
+    status: boolean,
+    nomor: number,
+    nama: string,
+    nama_latin: string,
+    jumlah_ayat: number,
+    tempat_turun: string,
+    arti: string,
+    deskripsi: string,
+    audio: string,
+    ayat: Array<AyahSurahTypes>,
+    surat_selanjutnya:ListDataSurahTypes,
+    surat_sebelumnya: ListDataSurahTypes
+
+  }
