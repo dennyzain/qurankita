e682b7df804a622f4c371be0ee4f82de4662314a
dennyzain <abbasdenny24@gmail.com>
2022-09-26 16:03:27 +0700

    refactor: add new state and action in store

--- src/stores/storeZustand.ts
+++ src/stores/storeZustand.ts
@@ -2,13 +2,21 @@
 import create from 'zustand';
 
 interface typesOfStore {
+  isDark:boolean;
   isSearch: boolean;
+  isCollapse: boolean;
   setIsSearch: (val: boolean) => void;
+  setIsCollapse: (val: boolean) => void;
+  setIsDark: (val: boolean) => void;
 }
 
 const useGlobalStore = create<typesOfStore>()((set) => ({
   isSearch: false,
+  isDark: false,
+  isCollapse: false,
   setIsSearch: (bool) => set(() => ({ isSearch: bool })),
+  setIsCollapse: (bool) => set(() => ({ isCollapse: bool })),
+  setIsDark: (bool) => set(() => ({ isDark: bool })),
 }));
 
 export default useGlobalStore;
